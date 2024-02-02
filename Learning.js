d46f98b3521a532e49c54eae45cab6598616f2d2
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-02-02 22:38:10 +0530

    Operator in JS

--- Learning.js
+++ Learning.js
@@ -1,7 +1,13 @@
 var a,b; //Declaration of variable
 let c; // variable block
-a = 9, b = 7, c = -91;
+a = 9, b = 2.22, c = 7;
 
 //Comments , /*..Multiline comments...*/
 
 console.log(a+b+c); //Printing
+
+console.log(a**b); // ** Exponential operator
+
+let word1 = "Dhiraj";
+console.log(word1 + " Prajapati"); // Concatinating two string
+console.log(typeof(word1), typeof(a), typeof(b)); // Give type of variable
